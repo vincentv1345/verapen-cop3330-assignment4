3b8221ec447f316cb1e0f9ea318777a156612e61
Vincent <vincentverapen2012@gmail.com>
2021-07-05 21:02:47 -0400

    Added Info

--- src/main/java/ucf/assignments/TodoLists.java
+++ src/main/java/ucf/assignments/TodoLists.java
@@ -1,9 +1,19 @@
 package ucf.assignments;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class TodoLists {
-    public static void main(String []args){
-        //start();
+import java.io.IOException;
+
+public class TodoLists extends Application{
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("todoListsController.fxml"));
+        primaryStage.setScene(new Scene(root, 300,275));
+        primaryStage.show();
     }
+    public static void main(String[] args){ launch(args); }
     class todoListMaker{
         //creates todoList using input from controller class
         //calls maketitle() method to make the todolist title
@@ -11,14 +21,32 @@ public class TodoLists {
         //calls additem() method to add items to list
     }
     class capacity extends todoListMaker{
-        //contains capacity method with has
-        // integer number of items for number of items
+        //int items initialized
+        //boolean containsItems(){
+        // if items is greater than 100 returns true
+        // else
+        // returns false
+        // }
         // contains add items method which adds items
         //using methods from the item class
     }
-    class title extends  todoListMaker{}
+    class title extends  todoListMaker{
+        //String makeTitle(){
+        // gets a title from the GUI
+        // }
+    }
     class item extends todoListMaker{
-
+        //initializes itemDescription
+        //initalizes String date
+        //Item obj equals new Item()
+        //String getCapacity(int capacity){
+        // capacityList equals capacity
+        // returns capacityList
+        // } void addItem(String item){
+        // getItem() gets item
+        // setItem() sets item
+        // itemDescription.setDescription()
+        // date.makeCurrentDate() gets new date
     }
     //import java time package
     class Date extends item{
---
+++ src/main/java/ucf/assignments/editItemController.java
@@ -0,0 +1,10 @@
+package ucf.assignments;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+public class editItemController extends TodoLists{
+    //@FXML initialize dueDateArray
+    // initialize descriptionArray
+    // initialize completion array
+
+}
